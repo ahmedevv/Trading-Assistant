2a4535b692ae62bb3b7bc4c9bdd0db7d78f0a46e
Muhammad Ahmad <learningalways1337@gmail.com>
2024-10-09 14:11:00 +0500

    Generlised the Fetch_data.py Code

--- fetch_data.py
+++ fetch_data.py
@@ -4,6 +4,27 @@ import datetime
 import pandas_ta as ta
 import datetime
 
+
+
+
+
+timeframe_dict = {
+                    'D' : mt5.TIMEFRAME_D1,
+                    'W' : mt5.TIMEFRAME_W1,
+                    'MN' : mt5.TIMEFRAME_MN1,
+                    'H12' : mt5.TIMEFRAME_H12,
+                    'H4' : mt5.TIMEFRAME_H4,
+                    'H2' : mt5.TIMEFRAME_H2,
+                    'H1' : mt5.TIMEFRAME_H1,
+                    'M30' : mt5.TIMEFRAME_M30,
+                    'M15' : mt5.TIMEFRAME_M15,
+                    'M5' : mt5.TIMEFRAME_M5,
+                    'M1' : mt5.TIMEFRAME_M1
+
+}
+
+
+
 def initializeMetatrader(path,login,password,servername):
         try:
             
@@ -17,23 +38,60 @@ def initializeMetatrader(path,login,password,servername):
             print('Check Metatrader Credentials i.e. Login,Password,Server')
 
 
-def getDailyData(symbol,path,login,password,servername):
+def timezoneTranslation(time_gap):
+
+    """
+        Given the time difference between different zones It'll convert the time to make sure the time is according to broker's time.
+
+    """
+
+    curr_time = datetime.datetime.now()
+    if time_gap == 0:
+        curr_time = datetime.datetime.now()
+    elif time_gap > 0:
+        curr_time = datetime.datetime.now()
+        curr_time = curr_time + datetime.timedelta(hours=time_gap)
+    elif time_gap < 0:
+        curr_time = datetime.datetime.now()
+        curr_time = curr_time - datetime.timedelta(hours=abs(time_gap))
+    return curr_time
+
+
+
+
+def getData(time_gap,timeframe,symbol,path,login,password,servername):
     try:
         initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-       
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_D1,0,500)
-        df = pd.DataFrame(daily)  
-        
+        data = mt5.copy_rates_from_pos(symbol,timeframe_dict.get(timeframe),0,500)
+        df = pd.DataFrame(data)  
+    
         df['time']  = pd.to_datetime(df['time'],unit='s')
         timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).day == timestamp.day:
-            df = df[:-1]
+        curr_time = timezoneTranslation(time_gap)
+        print(curr_time)
+ 
+    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
 
+        
+        if timeframe in ['H12','H4','H2','H1']:  
+
+            if curr_time.hour == timestamp.hour:
+                df = df[:-1]
+
+        elif timeframe in ['D','W']:
+           
+            if curr_time.day == timestamp.day:
+                df = df[:-1]
+
+        elif timeframe in ['MN'] :
+           
+            if curr_time.month == timestamp.month:
+                df = df[:-1]
+
+        elif timeframe in ['M30','M15','5M','M1'] :
+           
+            if curr_time.month == timestamp.month:
+                df = df[:-1]
 
 
         return df
@@ -44,161 +102,9 @@ def getDailyData(symbol,path,login,password,servername):
          
 
    
-def getMonthlyData(symbol,path,login,password,servername):
-    try:
-        initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        today = False
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-        hour = datetime.datetime.now(gmt2).hour
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_MN1,0,500)
-        df = pd.DataFrame(daily)  
-        
-        df['time']  = pd.to_datetime(df['time'],unit='s')
-        timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).month == timestamp.month:
-            today = True
-            df = df[:-1]
 
-
-
-        return df,today
-    except Exception as e:
-        
-        print(e)
     
 
-
-def getWeeklyData(symbol,path,login,password,servername):
-    try:
-        initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        today = False
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-        hour = datetime.datetime.now(gmt2).hour
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_W1,0,500)
-        df = pd.DataFrame(daily)  
-        
-        df['time']  = pd.to_datetime(df['time'],unit='s')
-        timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).day == timestamp.day:
-            today = True
-            df = df[:-1]
-
-
-
-        return df
-    except Exception as e:
-        
-        print(e)
-    
-
-def get4hData(symbol,path,login,password,servername):
-    try:
-        initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        today = False
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-        hour = datetime.datetime.now(gmt2).hour
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_H4,0,500)
-        df = pd.DataFrame(daily)  
-        
-        df['time']  = pd.to_datetime(df['time'],unit='s')
-        timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).hour == timestamp.hour:
-            today = True
-            df = df[:-1]
-
-
-
-        return df
-    except Exception as e:
-        
-        print(e)
-    
-
-
-def get1HData(symbol,path,login,password,servername):
-    try:
-        initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        today = False
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-        hour = datetime.datetime.now(gmt2).hour
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_H1,0,500)
-        df = pd.DataFrame(daily)  
-        
-        df['time']  = pd.to_datetime(df['time'],unit='s')
-        timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).hour == timestamp.hour:
-            today = True
-            df = df[:-1]
-
-
-
-        return df
-    except Exception as e:
-        
-        print(e)
-    
-
-
-def get5MData(symbol,path,login,password,servername):
-    try:
-        initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        today = False
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-        hour = datetime.datetime.now(gmt2).hour
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_M5,0,500)
-        df = pd.DataFrame(daily)  
-        
-        df['time']  = pd.to_datetime(df['time'],unit='s')
-        timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).minute == timestamp.minute:
-            today = True
-            df = df[:-1]
-
-
-        return df
-    except Exception as e:
-        
-        print(e)
-
-
-
-def get15MData(symbol,path,login,password,servername):
-    try:
-        initializeMetatrader(path,login,password,servername)
-        gmt2 = datetime.timezone(datetime.timedelta(hours=3), "GMT+3")
-        today = False
-    # the name "GMT+2" is optional, the name does not appear in output of isoformat, only the offset
-        hour = datetime.datetime.now(gmt2).hour
-        
-           
-        daily = mt5.copy_rates_from_pos(symbol,mt5.TIMEFRAME_M15,0,500)
-        df = pd.DataFrame(daily)  
-        
-        df['time']  = pd.to_datetime(df['time'],unit='s')
-        timestamp = df['time'].iloc[-1]
-        if datetime.datetime.now(gmt2).minute == timestamp.minute:
-            today = True
-            df = df[:-1]
-
-
-
-        return df
-    except Exception as e:
-        
-        print(e)
-    
+# Useage Example
+# df = getData(-4,'M5','EURUSDx','C:/Program Files/Metatrader 5/terminal64.exe',5466445487,'XCdd!!9855','NoorCapital-Server')
+# print(df)
